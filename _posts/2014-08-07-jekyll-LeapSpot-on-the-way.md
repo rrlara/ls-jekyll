d7c327a879244dce96662d5e1ba0433f28f86280
rrlara <rrlara@gmail.com>
2014-08-07 22:24:48 -0700

    Added profile image and name

---
+++ _posts/2014-08-07-jekyll-LeapSpot-on-the-way.md
@@ -0,0 +1,9 @@
+---
+layout: blog
+title: jekyll-LeapSpot-on-the-way
+category: blog
+lat: 47.66855
+lng: -122.38766
+altitude: 14.59
+image: https://s3-us-west-2.amazonaws.com/worldcup14/2014-08-07 18:39:03 PDT.jpg
+---
--- _site/json2markdown.py
+++ _site/json2markdown.py
@@ -3,75 +3,49 @@ import json
 
 
 
-# url = urllib2.urlopen('https://s3-us-west-2.amazonaws.com/worldcup14/Observations.json')
-# json_str = url.read()
-# json_str = json_str.replace("(null),", "").replace("(null)", "")
-# data = json.loads(json_str)   
+url = urllib2.urlopen('https://s3-us-west-2.amazonaws.com/worldcup14/Observations.json')
+json_str = url.read()
+json_str = json_str.replace("(null),", "").replace("(null)", "")
+ 
+print json_str 
 
-with open("SEA.json", 'rb') as f:
+json_str = json_str.replace("(null),", "").replace("(null)", "")
+print json_str
+data = json.loads(json_str)
 
-	json_str = f.read()
-	json_str = json_str.replace("(null),", "").replace("(null)", "")
-	print json_str
-	data = json.loads(json_str)
+for feature in data['features']:
 
-	for feature in data['features']:
+	comment = feature['properties']['comment']
+	comment = comment.replace(" ", "-")
 
-		comment = feature['properties']['comment']
-		comment = comment.replace(" ", "-")
+	timestamp = (feature['properties']['timestamp']).split(' ')[0]
 
-		timestamp = (feature['properties']['timestamp']).split(' ')[0]
+	altitude = feature['properties']['altitude']
 
-		# altitude = feature['properties']['altitude']
+	image = (feature['properties']['timestamp'])
 
-		image = (feature['properties']['timestamp'])
+	file_name = timestamp + "-" + comment
 
-		file_name = timestamp + "-" + comment
+	lat = feature['geometry']['coordinates'][1]
+	lng = feature['geometry']['coordinates'][0]
 
-		lat = feature['geometry']['coordinates'][1]
-		lng = feature['geometry']['coordinates'][0]
-
-		
-		print file_name
-		with open("_posts/" + file_name + '.md', 'wb') as out_file:
-			# out_file.write(str(feature))
-			out_file.write("---")
-			out_file.write("\n")
-			out_file.write("layout: blog")
-			out_file.write("\n")
-			out_file.write("title: " + comment.replace("-,", " "))
-			out_file.write("\n")
-			out_file.write("category: blog")
-			out_file.write("\n")
-			out_file.write("lat: " + str(lat))
-			out_file.write("\n")
-			out_file.write("lng: " + str(lng))
-			# out_file.write("\n")
-			# out_file.write("altitude: " + str(altitude))
-			out_file.write("\n")
-			out_file.write("image: https://s3-us-west-2.amazonaws.com/travels2013/" + str(image) + ".jpg")
-			out_file.write("\n")
-			out_file.write("---")
-
-
-
-
-
-
-
-# ---
-# layout: blog
-# published: true
-# title: "Tackling food security and nutrition in Egypt: challenges and opportunities"
-# category: event
-# comments: false
-# header: "Tackling food security and nutrition in Egypt: challenges and opportunities"
-# eventdate: 
-#   - "06/18/2014 - 06/19/2014"
-# location: 
-#   - Portland, OR
-# author: 
-#   - "Perrihan Al-Riffai"
-# agenda: "http://www.ifpri.org/sites/default/files/cairoagenda20130521.pdf"
-# powerpoint: "http://www.slideshare.net/ifpri/tackling-egypts-rising-food-insecurity-in-times-of-transition"
-# ---
+	
+	print file_name
+	with open("_posts/" + file_name + '.md', 'wb') as out_file:
+		out_file.write("---")
+		out_file.write("\n")
+		out_file.write("layout: blog")
+		out_file.write("\n")
+		out_file.write("title: " + comment.replace("-,", " "))
+		out_file.write("\n")
+		out_file.write("category: blog")
+		out_file.write("\n")
+		out_file.write("lat: " + str(lat))
+		out_file.write("\n")
+		out_file.write("lng: " + str(lng))
+		out_file.write("\n")
+		out_file.write("altitude: " + str(altitude))
+		out_file.write("\n")
+		out_file.write("image: https://s3-us-west-2.amazonaws.com/worldcup14/" + str(image) + ".jpg")
+		out_file.write("\n")
+		out_file.write("---")
